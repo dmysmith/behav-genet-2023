e66d29bc08a38aeec68a4928f48de10af4816397
Diana Smith <dmysmith@gmail.com>
2022-10-06 15:59:42 -0700

    clean documentation in create_twin_grm.m

--- create_twin_grm.m
+++ create_twin_grm.m
@@ -1,17 +1,20 @@
 %%%%%%%%%%%%%%%%%%%%%
-% Create dummy GRM for twin analyses
+% Create matrix of assigned GRM
 % Diana Smith
 % Aug 2022
 
+% Add path to cmig_tools (available from:
+% https://github.com/cmig-research-group/cmig_tools )
 addpath(genpath('/home/d9smith/github/cmig_tools/cmig_tools_utils/matlab'));
+
+% Path where I downloaded some matlab helper functions -- may not be needed for other users
 addpath(genpath('/home/d9smith/.matlab'));
 
 % Load full GRM file
-grm_file = '/space/amdale/1/tmp/ABCD_cache/abcd-sync/4.0/genomics/ABCD_rel4.0_grm.mat';
+grm_file = '/space/abcd-sync/4.0/genomics/ABCD_rel4.0_grm.mat';
 load(grm_file);
 
 % load twin ID list
-% twin_file = '/space/syn50/1/data/ABCD/d9smith/random_effects/behavioral/data/pheno/twins_ids_for_grm.txt';
 twin_file = "/home/d9smith/projects/random_effects/behavioral/twinfiles/ABCD_twins_all.txt";
 twin = readtable(twin_file);
 twin = twin(:,["IID1", "IID2", "twin1_genetic_zygosity"]);
@@ -60,24 +63,13 @@ end
 outfile = '/space/syn50/1/data/ABCD/d9smith/random_effects/behavioral/data/twins_assigned_grm.mat';
 save(outfile, 'GRM', 'iid_list');
 
-twin_grm_file = "/home/d9smith/projects/random_effects/behavioral/twinfiles/twins_measured_grm.txt";
-writetable(twin, twin_grm_file);
-
 GRM = GRM_assigned;
 assigned_GRM_file = '/space/syn50/1/data/ABCD/d9smith/random_effects/behavioral/data/all_discrete_grm.mat'; 
 save(assigned_GRM_file, 'GRM','iid_list')
 
 % for paper -- what is the distribution of GRM in the sample without twin site pts?
-
 for grmi = 1:size(GRM_notwin,1)
     GRM_notwin(grmi,grmi) = NaN;
 end
 
 disp(sum(GRM_notwin(:) >= 0.9));
-if 0 % code to generate histogram
-    figure('visible','off');clf; 
-    histogram(GRM_notwin, 300);
-    ylim([0 2000]);
-    export_fig(gcf, '/home/d9smith/tmp/grm_notwin.png');
-end
-
